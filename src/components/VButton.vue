be07014928c055d0c2b9869cc8dca08d69425a83
David Royer <droyer01@gmail.com>
2021-07-03 03:35:06 -0400

    refactor: style changes

--- src/components/VButton.vue
+++ src/components/VButton.vue
@@ -25,10 +25,10 @@ export default {
 
 <style lang="postcss">
   .btn {
-    @apply inline-flex justify-center items-center
+    @apply inline-flex justify-center items-center y-2 px-4 font-semibold rounded-lg shadow
   }
 
   .btn-blue {
-    @apply py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75;
+    @apply bg-blue-500 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75
   }
 </style>
